4b4cf8f33c9b0b3e7403d964bb0bfaa49a8a39fe
Bul Getir <bulvegetir@gmail.com>
2026-02-06 21:42:48 +0300

    Remove old postcss.config.js, keep CommonJS config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,9 @@
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    autoprefixer(),
+    cssnano({ preset: 'default' })
   ]
 };
