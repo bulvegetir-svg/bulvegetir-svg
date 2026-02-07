10ff0005fd38b1a4974563aefab9411d93742ebe
Bul Getir <bulvegetir@gmail.com>
2026-02-07 21:16:27 +0300

    index.njk eklendi ve build pipeline düzeltildi

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,15 +1,8 @@
-/* .eleventy.cjs – Eleventy konfigürasyonu (CommonJS) */
-module.exports = function (eleventyConfig) {
-  // Statik dosyaları kopyala (assets klasörü)
-  eleventyConfig.addPassthroughCopy("src/assets");
-
-  return {
-    dir: {
-      input: "src/pages",   // sayfa şablonları burada
-      includes: ".",        // <‑‑ Layout ve include dosyaları aynı klasörde (src/pages)
-      output: "dist"
-    },
-    templateFormats: ["njk", "html"],
-    htmlTemplateEngine: "njk"
-  };
+export default {
+  dir: {
+    input: "src",
+    includes: "_includes",
+    layouts: "_includes",
+    output: "dist"
+  }
 };
