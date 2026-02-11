2e973657a81333eb3b89aba77f7a40aef2bbe61b
Bul Getir <bulvegetir@gmail.com>
2026-02-11 23:19:25 +0300

    Eleventy config fix and Vercel deployment

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
-// .eleventy.js  (SON ve TEMİZ)
+// .eleventy.js (GÜNCELLENMİŞ YOL)
+import fs from "fs";
 
 export default function (eleventyConfig) {
   // Statik dosyalar
   eleventyConfig.addPassthroughCopy({
     "src/assets": "assets",
   });
+
+  // --- DÜZELTİLEN ÇÖZÜM: Ürün koleksiyonu ---
+  eleventyConfig.addCollection("products", () => {
+    // Yolu _data olarak düzelttik
+    const data = JSON.parse(fs.readFileSync("./src/_data/products.json", "utf-8"));
+    return data.products;
+  });
+  // ----------------------------------------
 
   // TL fiyat filtresi
   eleventyConfig.addFilter("currency", (num) => {
