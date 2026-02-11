35906d39b8f473c920658a8d008ab2d69e762223
Bul Getir <bulvegetir@gmail.com>
2026-02-12 02:08:23 +0300

    Fix: Added search-data.xml to passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
-// .eleventy.js (GÜNCELLENMİŞ YOL)
+// .eleventy.js
 import fs from "fs";
 
 export default function (eleventyConfig) {
-  // Statik dosyalar
+  // Statik dosyalar ve XML dosyası
   eleventyConfig.addPassthroughCopy({
     "src/assets": "assets",
+    "src/search-data.xml": "search-data.xml" // Yeni eklenen satır
   });
 
-  // --- DÜZELTİLEN ÇÖZÜM: Ürün koleksiyonu ---
+  // --- Ürün koleksiyonu ---
   eleventyConfig.addCollection("products", () => {
-    // Yolu _data olarak düzelttik
     const data = JSON.parse(fs.readFileSync("./src/_data/products.json", "utf-8"));
     return data.products;
   });
-  // ----------------------------------------
+  // ------------------------
 
   // TL fiyat filtresi
   eleventyConfig.addFilter("currency", (num) => {
